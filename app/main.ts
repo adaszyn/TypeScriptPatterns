805f2460f8d101681e8e941d84a5fbafd9c0a9f2
wojciech <wojciech.adaszynski@gmail.com>
2016-01-19 22:59:17 +0100

    Strategy pattern, fixed project structure

--- app/main.ts
+++ app/main.ts
@@ -1,23 +1,13 @@
-import {myModule} from "./my_module"
-import {CarModule} from "./builder/CarModule"
-import {Singleton} from "./singleton/Singleton"
-import {PizzaModule} from "./factoryMethod/PizzaModule"
-myModule();
-var carSalesman = new CarModule.CarSalesman();
-var pussyWagonBuilder = new CarModule.PussyWagonBuilder();
-carSalesman.carBuilder = pussyWagonBuilder;
-carSalesman.constructCar();
-var car = carSalesman.getCar();
-var riri = new Singleton.Rihanna();
-try {
-	var riri2 = new Singleton.Rihanna();
-}
-catch (error) {
-	console.log('Exception thrown!');
-}
-// console.log(riri.needs());
-// console.log(riri2.needs());
+import {Strategy} from "./behavioral/strategy/Strategy"
 
-var PizzaFactory = new PizzaModule.HawaiianPizzaFactory();
-var pizza = PizzaFactory.getPizza();
-console.log(pizza.getType());
+class Main {
+    public static main(): any {
+        console.log("Running script");
+        var array: Array<number> = [1, 5, 2, 6, 10, 1, -100, 12, 2];
+        var strategy = new Strategy();
+        console.log(array);
+        strategy.sort(array);
+        console.log(array);
+    }
+}
+Main.main();
--- app/my_module.ts
+++
@@ -1,3 +0,0 @@
-export function myModule() {
-	console.log("message from mymodule");
-}
